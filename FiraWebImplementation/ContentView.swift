7334635d9e4e1dd702e4d1236566f8cf1e881a89
Miguel Rincon <miguelrincon@protonmail.com>
2021-12-20 15:30:06 -0500

    First commit

--- FiraWebImplementation/ContentView.swift
+++ FiraWebImplementation/ContentView.swift
@@ -8,9 +8,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var isShowingWebView = false
+    
+    var url = URL(string: "https://firalivedev.blob.core.windows.net/scripts/demo/index.html?store=61086e62e714cd300dfedf85")!
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            NavigationLink(destination: SafariView(url: url)) {
+                Text("show web view")
+            }
+        }
+//        Button(action: showWebView) {
+//            Text("show safari view")
+//        }
+//        .sheet(isPresented: $isShowingWebView) {
+//            WebView(url: URL(string: firaUrl)!)
+//        }
+    }
+    
+    func showWebView() {
+        isShowingWebView.toggle()
     }
 }
 
